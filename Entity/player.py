a9906aaafbc00c9cc5f9560b1d1c6c61114c9f58
DejanLivada <dejli16@majkajugovica.edu.rsm>
2022-05-05 19:36:09 +0200

    try fix error

---
+++ Entity/player.py
@@ -0,0 +1,8 @@
+import pickle
+class Player:
+    def __init__(self , name , health , slika):
+        self.slika = slika
+        self.name = name
+        self.health = health
+player_izabrana_slika = None
+
